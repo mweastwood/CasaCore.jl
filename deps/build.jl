d1e7719383fb39501dd1a0e68b67e24ece1fbe9b
Michael Eastwood <mweastwood@astro.caltech.edu>
2014-12-04 23:47:05 -0800

    First attempt at getting CasaCore to build on Travis

--- deps/build.jl
+++ deps/build.jl
@@ -1,18 +1,38 @@
 using BinDeps
 @BinDeps.setup
 
-# For now we will assume that CasaCore has been built.
-# However, we still need to download the header files
-# from CasaCore to compile the wrapper.
-
 version = "1.7.0"
 url = "ftp://ftp.atnf.csiro.au/pub/software/casacore/casacore-$version.tar.bz2"
 
+libcasa_tables     = library_dependency("libcasa_tables")
+libcasa_measures   = library_dependency("libcasa_measures")
 libcasacorewrapper = library_dependency("libcasacorewrapper")
-provides(Sources, URI(url), libcasacorewrapper, unpacked_dir="casacore-$version")
+casacore_libraries = [libcasa_tables,libcasa_measures]
+all_libraries = [casacore_libraries, libcasacorewrapper]
 
 depsdir  = BinDeps.depsdir(libcasacorewrapper)
 prefix   = joinpath(depsdir,"usr")
+
+provides(Sources, URI(url), all_libraries, unpacked_dir="casacore-$version")
+
+srcdir   = joinpath(depsdir,"src",   "casacore-$version")
+builddir = joinpath(depsdir,"builds","casacore-$version")
+files    = [joinpath(prefix,"lib",library.name*".so") for library in casacore_libraries]
+provides(BuildProcess,
+        (@build_steps begin
+                GetSources(libcasa_tables)
+                GetSources(libcasa_measures)
+                CreateDirectory(builddir)
+                @build_steps begin
+                        ChangeDirectory(builddir)
+                        FileRule(files,@build_steps begin
+                                `cmake -DCMAKE_INSTALL_PREFIX $prefix $srcdir`
+                                `make`
+                                `make install`
+                        end)
+                end
+        end),casacore_libraries)
+
 builddir = joinpath(depsdir,"builds","casacorewrapper")
 provides(BuildProcess,
         (@build_steps begin
