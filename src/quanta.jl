35b0cead7f9b45b795034c62e7bd6a81b61d355e
Michael Eastwood <mweastwood@astro.caltech.edu>
2014-12-16 09:57:04 -0800

    Improve the q_str macro and add tests for quantity construction

--- src/quanta.jl
+++ src/quanta.jl
@@ -5,6 +5,8 @@ end
 
 Quantity(record::CasaRecord) = Quantity(record["value"],record["unit"])
 
+==(q1::Quantity,q2::Quantity) = (q1.value == q2.value) && (q1.unit == q2.unit)
+
 @doc """
 This macro constructs a Quantity from a string.
     
@@ -12,8 +14,26 @@ This macro constructs a Quantity from a string.
 * q"1234.5s" → Quantity(1234.5,"s")
 * q"1.23e4s" → Quantity(1.23e4,"s")
 * q"1.23rad" → Quantity(1.23,"rad")
+* q"12h00m00s" → Quantity(float(π),"rad")
 """ ->
 macro q_str(string)
+    # Check for hour angle format
+    regex = r"([0-9]?[0-9]\.?[0-9]*)h"
+    if match(regex,string) != nothing
+        substrs = match(regex,string).captures
+        return Quantity(float(substrs[1])*π/12.,"rad")
+    end
+    regex = r"([0-9]?[0-9])h([0-9]?[0-9]\.?[0-9]*)m"
+    if match(regex,string) != nothing
+        substrs = match(regex,string).captures
+        return Quantity((float(substrs[1])+float(substrs[2])/60.)*π/12.,"rad")
+    end
+    regex = r"([0-9]?[0-9])h([0-9]?[0-9])m([0-9]?[0-9]\.?[0-9]*)s"
+    if match(regex,string) != nothing
+        substrs = match(regex,string).captures
+        return Quantity((float(substrs[1])+float(substrs[2])/60.+float(substrs[3])/3600.)*π/12.,"rad")
+    end
+    # Check for generic unitful numbers
     regex   = r"([0-9]+\.?[0-9]*e?[0-9]*)([A-Za-z]+)"
     substrs = match(regex,string).captures
     Quantity(float(substrs[1]),ASCIIString(substrs[2]))
