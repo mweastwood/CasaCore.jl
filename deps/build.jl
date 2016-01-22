9fa85ee08a806897b01f5fb8e953551c19af07c6
Michael Eastwood <mweastwood@astro.caltech.edu>
2016-01-22 11:02:40 -0800

    fix Travis build
    It looks like the casacore header files were out of sync with the
    casacore libraries. Previously I was grabbing the header files from
    github and the libraries from a ppa. Now I get them both from the ppa.

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,6 @@
 depsdir = dirname(@__FILE__)
 src = joinpath(depsdir,"src")
 
-# Download the CasaCore source
-println("Downloading the CasaCore source...")
-run(`git clone --branch v2.0.3 https://github.com/casacore/casacore`)
-
 # Build the CasaCore wrapper
 println("Building the CasaCore wrapper...")
 run(`make -C $src`)
