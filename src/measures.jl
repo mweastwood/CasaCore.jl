c507459cc8c6c91a0d665c08352c6ac761260e8f
Michael Eastwood <mweastwood@astro.caltech.edu>
2016-10-19 18:14:11 -0700

    switch from my crappy units implementation to Unitful
    I'm still not entirely happy with this because Unitful encourages people
    to edit their unit definitions at the package level, which means
    `using Unitful` can have different results depending on the machine.
    Therefore you can't even reliably expect that there are units for a day,
    a second, or any time units at all.
    
    This is not really acceptable so this needs to change before I merge
    this into master.
    
    I've brought up the issue at
    https://github.com/ajkeller34/Unitful.jl/issues/33
    
    The likely outcome of this is that I will maintain an AstroUnits package
    that will extend Unitful and provide a good set of units that are
    suitable for general use in astronomy. CasaCore will then depend on that
    package instead.

--- src/measures.jl
+++ src/measures.jl
@@ -26,6 +26,12 @@ export seconds, days, degrees, radians, meters
 
 using ..Common
 
+using Unitful
+# See https://github.com/ajkeller34/Unitful.jl/issues/38 for a discussion of angle units in the
+# Unitful package. We decided that it makes sense for angles to be dimensionless, but Andrew was
+# hesitant to commit to this typealias within Unitful.
+typealias Angle{T} Unitful.DimensionlessQuantity{T}
+
 const libcasacorewrapper = joinpath(dirname(@__FILE__),"../deps/libcasacorewrapper.so")
 isfile(libcasacorewrapper) || error("Run Pkg.build(\"CasaCore\")")
 
@@ -59,44 +65,6 @@ module Baselines
     const AZELNE = AZEL
     const AZELNEGEO = AZELGEO
 end
-
-"""
-This module provides basic support for dispatching on units.
-This is intended *only* for interacting with CasaCore.Measures.
-
-Todo: Replace this with SIUnits once that package gets the
-love and affection it clearly needs.
-"""
-module Units
-    abstract Unit
-    abstract Time <: Unit
-    abstract Angle <: Unit
-    abstract Distance <: Unit
-
-    immutable Second <: Time val :: Float64 end
-    immutable Day    <: Time val :: Float64 end
-    Second(x::Second) = x
-    Second(x::Day) = Second(x.val * 24*60*60)
-    Day(x::Second) = Day(x.val / (24*60*60))
-
-    immutable Degree <: Angle val :: Float64 end
-    immutable Radian <: Angle val :: Float64 end
-    Radian(x::Radian) = x
-    Radian(x::Degree) = Radian(x.val * π/180)
-
-    immutable Meter <: Distance val :: Float64 end
-    Meter(x::Meter) = x
-
-    importall Base.Operators
-    *{T<:Unit}(num::Real, unit::T) = T(num*unit.val)
-    value(unit::Unit) = unit.val
-end
-
-const seconds = Units.Second(1.0)
-const days    = Units.Day(1.0)
-const degrees = Units.Degree(1.0)
-const radians = Units.Radian(1.0)
-const meters  = Units.Meter(1.0)
 
 macro wrap(expr)
     jl_name  = expr.args[2].args[1]
@@ -132,11 +100,31 @@ macro wrap(expr)
     end |> esc
 end
 
+"""
+    ReferenceFrame
+
+The `ReferenceFrame` type contains information about the frame of reference to use when converting
+between coordinate systems. For example converting from J2000 coordinates to AZEL coordinates
+requires knowledge of the observer's location, and the current time. However converting between
+B1950 coordinates and J2000 coordinates requires no additional information about the observer's
+frame of reference.
+
+Use the `set!` function to add information to the given frame of reference.
+
+**Example:**
+
+``` julia
+frame = ReferenceFrame()
+set!(frame, observatory("VLA")) # set the observer's position to the location of the VLA
+set!(frame, Epoch(epoch"UTC", 50237.29days)) # set the current UTC time
+```
+"""
 @wrap_pointer ReferenceFrame
+
 abstract Measure
 
 """
-    immutable Epoch <: Measure
+    Epoch <: Measure
 
 This type represents an instance in time.
 """
@@ -146,7 +134,7 @@ This type represents an instance in time.
 end
 
 """
-    immutable Direction <: Measure
+    Direction <: Measure
 
 This type represents a location on the sky.
 """
@@ -158,7 +146,7 @@ This type represents a location on the sky.
 end
 
 """
-    immutable Position <: Measure
+    Position <: Measure
 
 This type represents a location on the surface of the Earth.
 """
@@ -170,7 +158,7 @@ This type represents a location on the surface of the Earth.
 end
 
 """
-    immutable Baseline <: Measure
+    Baseline <: Measure
 
 This type represents the location of one antenna relative to another antenna.
 """
@@ -184,88 +172,155 @@ end
 """
     Epoch(sys, time)
 
-Instantiate an epoch in the given coordinate system.
+Instantiate an epoch in the given coordinate system (`sys`).
 
-Note that the time should be a modified Julian date.
+The `time` should be given as a modified Julian date.  Additionally the Unitful package should be
+used to communicate the units of `time`.
+
+For example `time = 57365.5 * u"d"` corresponds to a Julian date of 57365.5 days. However you can
+also specify the Julian date in seconds (`u"s"`), or any other unit of time supported by Unitful.
+
+**Coordinate Systems:**
+
+The coordinate system is selected using the string macro `epoch"..."` where the `...` is replaced
+with one of the coordinate systems listed below.
+
+* `LAST` - local apparent sidereal time
+* `LMST` - local mean sidereal time
+* `GMST1` - Greenwich mean sidereal time
+* `GAST` - Greenwich apparent sidereal time
+* `UT1` - UT0 (raw time from GPS measurements) corrected for polar wandering
+* `UT2` - UT1 corrected for variable Earth rotation
+* `UTC` - coordinated universal time
+* `TAI` - international atomic time
+* `TDT` - terrestrial dynamical time
+* `TCG` - geocentric coordinate time
+* `TDB` - barycentric dynamical time
+* `TCB` - barycentric coordinate time
 
 **Examples:**
 
-    Epoch(epoch"UTC",     0.0days) # 1858-11-17T00:00:00
-    Epoch(epoch"UTC", 57365.5days) # 2015-12-09T12:00:00
+``` julia
+using Unitful: d
+Epoch(epoch"UTC",     0.0d) # 1858-11-17T00:00:00
+Epoch(epoch"UTC", 57365.5d) # 2015-12-09T12:00:00
+```
 """
-function Epoch(sys::Epochs.System, time)
-    seconds = Units.Second(time) |> Units.value
+function Epoch(sys::Epochs.System, time::Unitful.Time)
+    seconds = uconvert(u"s", time) |> ustrip
     Epoch(sys, seconds)
 end
 
 """
     Direction(sys, longitude, latitude)
+    Direction(sys)
 
-Instantiate a direction in the given coordinate system.
+Instantiate a direction in the given coordinate system (`sys`).
 
-Note that the longitude and latitude should be given as sexagesimal strings.
+The longitude and latitude may either be a sexagesimally formatted string, or an angle where the
+units (degrees or radians) are specified by using the Unitful package. If the longitude and latitude
+coordinates are not provided, they are assumed to be zero.
+
+**Coordinate Systems:**
+
+The coordinate system is selected using the string macro `dir"..."` where the `...` is replaced with
+one of the coordinate systems listed below.
+
+* `J2000` - mean equator and equinox at J2000.0 (FK5)
+* `JMEAN` - mean equator and equinox at frame epoch
+* `JTRUE` - true equator and equinox at frame epoch
+* `APP` - apparent geocentric position
+* `B1950` - mean epoch and ecliptic at B1950.0
+* `B1950_VLA` - mean epoch (1979.9) and ecliptic at B1950.0
+* `BMEAN` - mean equator and equinox at frame epoch
+* `BTRUE` - true equator and equinox at frame epoch
+* `GALACTIC` - galactic coordinates
+* `HADEC` - topocentric hour angle and declination
+* `AZEL` - topocentric azimuth and elevation (N through E)
+* `AZELSW` - topocentric azimuth and elevation (S through W)
+* `AZELGEO` - geodetic azimuth and elevation (N through E)
+* `AZELSWGEO` - geodetic azimuth and elevation (S through W)
+* `JNAT` - geocentric natural frame
+* `ECLIPTIC` - ecliptic for J2000 equator and equinox
+* `MECLIPTIC` - ecliptic for mean equator of date
+* `TECLIPTIC` - ecliptic for true equator of date
+* `SUPERGAL` - supergalactic coordinates
+* `ITRF` - coordinates with respect to the ITRF Earth frame
+* `TOPO` - apparent topocentric position
+* `ICRS` - international celestial reference system
+* `MERCURY`
+* `VENUS`
+* `MARS`
+* `JUPITER`
+* `SATURN`
+* `URANUS`
+* `NEPTUNE`
+* `PLUTO`
+* `SUN`
+* `MOON`
 
 **Examples:**
 
-    Direction(dir"AZEL", 0degrees, 90degrees) # topocentric zenith
-    Direction(dir"J2000", "12h00m", "43d21m")
+``` julia
+using Unitful: °, rad
+Direction(dir"AZEL", 0°, 90°) # topocentric zenith
+Direction(dir"ITRF", 0rad, 1rad)
+Direction(dir"J2000", "12h00m", "43d21m")
+Direction(dir"SUN")     # the direction towards the Sun
+Direction(dir"JUPITER") # the direction towards Jupiter
+```
 """
-function Direction(sys::Directions.System, longitude::Units.Angle, latitude::Units.Angle)
-    long = Units.Radian(longitude) |> Units.value
-    lat  = Units.Radian( latitude) |> Units.value
+function Direction(sys::Directions.System, longitude::Angle, latitude::Angle)
+    long = uconvert(u"rad", longitude) |> ustrip
+    lat  = uconvert(u"rad",  latitude) |> ustrip
     (ccall(("newDirection_longlat",libcasacorewrapper), Ptr{Void},
            (Cint, Float64, Float64), sys, long, lat) |> Direction_cxx |> to_julia) :: Direction
 end
 
 function Direction(sys::Directions.System, longitude::AbstractString, latitude::AbstractString)
-    Direction(sys, sexagesimal(longitude)*radians, sexagesimal(latitude)*radians)
+    Direction(sys, sexagesimal(longitude)*u"rad", sexagesimal(latitude)*u"rad")
 end
 
-"""
-    Direction(sys)
-
-Instantiate a direction with the given coordinate system. The longitude
-and latitude are set to zero.
-
-This constructor should be used for solar system objects.
-
-** Examples:**
-
-    Direction(dir"SUN")     # the direction towards the Sun
-    Direction(dir"JUPITER") # the direction towards Jupiter
-"""
-function Direction(sys::Directions.System)
-    Direction(sys, 1.0, 0.0, 0.0)
-end
+Direction(sys::Directions.System) = Direction(sys, 1.0, 0.0, 0.0)
 
 """
     Position(sys, elevation, longitude, latitude)
 
-Instantiate a position in the given coordinate system.
+Instantiate a position in the given coordinate system (`sys`).
 
-Note that depending on the coordinate system the elevation
-may be measured relative to the center or the surface of the Earth.
-In both cases it should be specified in meters.
+Note that depending on the coordinate system the elevation may be measured relative to the center or
+the surface of the Earth.  In both cases the units should be given with the Unitful package.  The
+longitude and latitude may either be a sexagesimally formatted string, or an angle where the units
+(degrees or radians) are specified by using the Unitful package. If the longitude and latitude
+coordinates are not provided, they are assumed to be zero.
 
-The longitude and latitude should be given as sexagesimal strings.
+**Coordinate Systems:**
+
+The coordinate system is selected using the string macro `pos"..."` where the `...` is replaced with
+one of the coordinate systems listed below.
+
+* `ITRF` - the International Terrestrial Reference Frame
+* `WGS84` - the World Geodetic System 1984
 
 **Examples:**
 
-    Position(pos"WGS84", 5_000meters, "20d30m00s", "-80d00m00s")
-    Position(pos"WGS84", 5_000meters, 20.5degrees, -80degrees)
+``` julia
+using Unitful: m, °
+Position(pos"WGS84", 5000m, "20d30m00s", "-80d00m00s")
+Position(pos"WGS84", 5000m, 20.5°, -80°)
+```
 """
-function Position(sys::Positions.System, elevation::Units.Distance,
-                  longitude::Units.Angle, latitude::Units.Angle)
-    rad  = Units.Meter( elevation) |> Units.value
-    long = Units.Radian(longitude) |> Units.value
-    lat  = Units.Radian( latitude) |> Units.value
+function Position(sys::Positions.System, elevation::Unitful.Length, longitude::Angle, latitude::Angle)
+    rad  = uconvert(u"m", elevation) |> ustrip
+    long = uconvert(u"rad", longitude) |> ustrip
+    lat  = uconvert(u"rad",  latitude) |> ustrip
     (ccall(("newPosition_elevationlonglat",libcasacorewrapper), Ptr{Void},
            (Cint, Float64, Float64, Float64), sys, rad, long, lat) |> Position_cxx |> to_julia) :: Position
 end
 
-function Position(sys::Positions.System, elevation::Units.Distance,
+function Position(sys::Positions.System, elevation::Unitful.Length,
                   longitude::AbstractString, latitude::AbstractString)
-    Position(sys, elevation, sexagesimal(longitude)*radians, sexagesimal(latitude)*radians)
+    Position(sys, elevation, sexagesimal(longitude)*u"rad", sexagesimal(latitude)*u"rad")
 end
 
 function Base.show(io::IO, epoch::Epoch)
@@ -313,14 +368,16 @@ macro baseline_str(sys)
 end
 
 """
-    observatory(name::AbstractString)
+    observatory(name)
 
 Get the position of an observatory from its name.
 
 **Examples:**
 
-    observatory("VLA")  # the Very Large Array
-    observatory("ALMA") # the Atacama Large Millimeter/submillimeter Array
+``` julia
+observatory("VLA")  # the Very Large Array
+observatory("ALMA") # the Atacama Large Millimeter/submillimeter Array
+```
 """
 function observatory(name::AbstractString)
     position = Position(pos"ITRF", 0.0, 0.0, 0.0) |> Ref{Position}
@@ -331,17 +388,48 @@ function observatory(name::AbstractString)
 end
 
 """
+    measure(frame, value, newsys)
+
+Converts the value measured in the given frame of reference into a new coordinate system.
+
+**Arguments:**
+
+* `frame` - an instance of the `ReferenceFrame` type
+* `value` - an `Epoch`, `Direction`, or `Position` that will be converted from its current
+            coordinate system into the new one
+* `newsys` - the new coordinate system
+
+Note that the reference frame must have all the required information to convert between the
+coordinate systems. Not all conversions require the same information!
+
+**Examples:**
+
+``` julia
+# Compute the azimuth and elevation of the Sun
+measure(frame, Direction(dir"SUN"), dir"AZEL")
+
+# Compute the ITRF position of the VLA
+measure(frame, observatory("VLA"), pos"ITRF")
+
+# Compute the atomic time from a UTC time
+measure(frame, Epoch(epoch"UTC", 50237.29days), epoch"TAI")
+```
+"""
+measure
+
+"""
     sexagesimal(string)
 
 Parse angles given in sexagesimal format.
 
-The regular expression used here understands how to match
-hours and degrees.
+The regular expression used here understands how to match hours and degrees.
 
 **Examples:**
 
-    sexagesimal("12h34m56.7s")
-    sexagesimal("+12d34m56.7s")
+``` julia
+sexagesimal("12h34m56.7s")
+sexagesimal("+12d34m56.7s")
+```
 """
 function sexagesimal(str::AbstractString)
     # Explanation of the regular expression:
@@ -376,8 +464,12 @@ Construct a sexagesimal string from the given angle.
 * If `hours` is `true`, the constructed string will use hours instead of degrees.
 * `digits` specifies the number of decimal points to use for seconds/arcseconds.
 """
-function sexagesimal(angle; hours::Bool = false, digits::Int = 0)
-    radians = Units.Radian(angle) |> Units.value
+function sexagesimal{T}(angle::T; hours::Bool = false, digits::Int = 0)
+    if T <: Angle
+        radians = uconvert(u"rad", angle) |> ustrip
+    else
+        radians = angle
+    end
     if hours
         s = +1
         radians = mod2pi(radians)
