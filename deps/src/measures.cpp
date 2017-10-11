fee6ef21ccebd49ecb0bcdccc64b17cd3cef428e
Michael Eastwood <westplastic@gmail.com>
2017-10-11 16:46:43 -0700

    rework measures so that we're not storing Ptr{Void} everywhere (#65)
    Ptr{Void} doesn't play very nicely with Julia's GC because the GC
    doesn't know how large the object is that we are pointing to. Frequently
    it will prefer to do nothing rather than call the object's finalizer.
    
    See #63

--- deps/src/measures.cpp
+++ deps/src/measures.cpp
@@ -29,6 +29,8 @@
 using namespace std;
 using namespace casacore;
 
+// These structs must mirror their corresponding Julia types.
+
 struct Epoch {
     int sys;
     double time; // measured in seconds
@@ -55,95 +57,138 @@ struct Baseline {
     double z; // measured in meters
 };
 
+// In some cases we will want to use Julia's nullable types. These types have an extra bool. If the
+// definition of Julia's nullable types ever changes, these definitions will need to be updated.
+//
+// NOTE: In Julia v0.6 the `isnull` field changed to `hasvalue`.
+
+struct NullableEpoch {
+    bool isnull;
+    Epoch value;
+};
+
+struct NullableDirection {
+    bool isnull;
+    Direction value;
+};
+
+struct NullablePosition {
+    bool isnull;
+    Position value;
+};
+
+struct ReferenceFrame {
+    NullableEpoch epoch;
+    NullableDirection direction;
+    NullablePosition position;
+};
+
+// Define conversion routines from the C++ types to the Julia types.
+
+Epoch getEpoch(MEpoch const& mepoch) {
+    Epoch epoch;
+    epoch.sys  = mepoch.getRef().getType();
+    epoch.time = mepoch.get("s").getValue();
+    return epoch;
+}
+
+Direction getDirection(MDirection const& mdirection) {
+    Vector<Double> vec = mdirection.getValue().getVector();
+    Direction direction;
+    direction.sys = mdirection.getRef().getType();
+    direction.x = vec(0);
+    direction.y = vec(1);
+    direction.z = vec(2);
+    return direction;
+}
+
+Position getPosition(MPosition const& mposition) {
+    Vector<Double> vec = mposition.getValue().getVector();
+    Position position;
+    position.sys = mposition.getRef().getType();
+    position.x = vec(0);
+    position.y = vec(1);
+    position.z = vec(2);
+    return position;
+}
+
+Baseline getBaseline(MBaseline const& mbaseline) {
+    Vector<Double> vec = mbaseline.getValue().getVector();
+    Baseline baseline;
+    baseline.sys = mbaseline.getRef().getType();
+    baseline.x = vec(0);
+    baseline.y = vec(1);
+    baseline.z = vec(2);
+    return baseline;
+}
+
+// Define conversion routines from the Julia types to the C++ types.
+
+MEpoch getMEpoch(Epoch const& epoch) {
+    return MEpoch(Quantity(epoch.time, "s"), MEpoch::Ref(epoch.sys));
+}
+
+MDirection getMDirection(Direction const& direction) {
+    return MDirection(MVDirection(direction.x, direction.y, direction.z),
+                      MDirection::Ref(direction.sys));
+}
+
+MPosition getMPosition(Position const& position) {
+    return MPosition(MVPosition(position.x, position.y, position.z),
+                     MPosition::Ref(position.sys));
+}
+
+MBaseline getMBaseline(Baseline const& baseline) {
+    return MBaseline(MVBaseline(baseline.x, baseline.y, baseline.z),
+                     MBaseline::Ref(baseline.sys));
+}
+
+MeasFrame getMeasFrame(ReferenceFrame const& frame) {
+    MeasFrame mframe = MeasFrame();
+    if (!frame.epoch.isnull) {
+        MEpoch mepoch = getMEpoch(frame.epoch.value);
+        mframe.set(mepoch);
+    }
+    if (!frame.direction.isnull) {
+        MDirection mdirection = getMDirection(frame.direction.value);
+        mframe.set(mdirection);
+    }
+    if (!frame.position.isnull) {
+        MPosition mposition = getMPosition(frame.position.value);
+        mframe.set(mposition);
+    }
+    return mframe;
+}
+
 extern "C" {
-    MeasFrame* newReferenceFrame() {return new MeasFrame();}
-    void deleteReferenceFrame(MeasFrame* frame) {delete frame;}
-
-    MEpoch* newEpoch(Epoch epoch) {
-        return new MEpoch(Quantity(epoch.time, "s"), MEpoch::Ref(epoch.sys));
+    Epoch convertEpoch(Epoch* input, int newsys) {
+        MEpoch input_epoch = getMEpoch(*input);
+        MEpoch output_epoch = MEpoch::Convert(input_epoch, MEpoch::Ref(newsys))();
+        return getEpoch(output_epoch);
     }
 
-    MDirection* newDirection(Direction direction) {
-        return new MDirection(MVDirection(direction.x, direction.y, direction.z),
-                              MDirection::Ref(direction.sys));
-    }
-    MDirection* newDirection_longlat(int sys, double longitude, double latitude) {
-        return new MDirection(Quantity(longitude, "rad"),
-                              Quantity( latitude, "rad"), MDirection::Ref(sys));
-    }
-
-    MPosition* newPosition(Position position) {
-        return new MPosition(MVPosition(position.x, position.y, position.z),
-                             MPosition::Ref(position.sys));
-    }
-    MPosition* newPosition_elevationlonglat(int sys, double elevation, double longitude, double latitude) {
-        return new MPosition(Quantity(elevation, "m"),
-                             Quantity(longitude, "rad"),
-                             Quantity( latitude, "rad"), MPosition::Ref(sys));
+    Direction convertDirection(Direction* input, int newsys, ReferenceFrame* frame) {
+        MDirection input_direction = getMDirection(*input);
+        MeasFrame mframe = getMeasFrame(*frame);
+        MDirection::Ref ref = MDirection::Ref(newsys, mframe);
+        MDirection output_direction = MDirection::Convert(input_direction, ref)();
+        return getDirection(output_direction);
     }
 
-    MBaseline* newBaseline(Baseline baseline) {
-        return new MBaseline(MVBaseline(baseline.x, baseline.y, baseline.z),
-                             MBaseline::Ref(baseline.sys));
+    Position convertPosition(Position* input, int newsys, ReferenceFrame* frame) {
+        MPosition input_position = getMPosition(*input);
+        MeasFrame mframe = getMeasFrame(*frame);
+        MPosition::Ref ref = MPosition::Ref(newsys, mframe);
+        MPosition output_position = MPosition::Convert(input_position, ref)();
+        return getPosition(output_position);
     }
 
-    void     deleteEpoch(    MEpoch*     mepoch) {delete mepoch;}
-    void deleteDirection(MDirection* mdirection) {delete mdirection;}
-    void  deletePosition( MPosition*  mposition) {delete mposition;}
-    void  deleteBaseline( MBaseline*  mbaseline) {delete mbaseline;}
-
-    Epoch getEpoch(MEpoch* mepoch) {
-        Epoch epoch;
-        epoch.sys  = mepoch->getRef().getType();
-        epoch.time = mepoch->get("s").getValue();
-        return epoch;
-    }
-
-    Direction getDirection(MDirection* mdirection) {
-        Vector<Double> vec = mdirection->getValue().getVector();
-        Direction direction;
-        direction.sys = mdirection->getRef().getType();
-        direction.x = vec(0);
-        direction.y = vec(1);
-        direction.z = vec(2);
-        return direction;
-    }
-
-    Position getPosition(MPosition* mposition) {
-        Vector<Double> vec = mposition->getValue().getVector();
-        Position position;
-        position.sys = mposition->getRef().getType();
-        position.x = vec(0);
-        position.y = vec(1);
-        position.z = vec(2);
-        return position;
-    }
-
-    Baseline getBaseline(MBaseline* mbaseline) {
-        Vector<Double> vec = mbaseline->getValue().getVector();
-        Baseline baseline;
-        baseline.sys = mbaseline->getRef().getType();
-        baseline.x = vec(0);
-        baseline.y = vec(1);
-        baseline.z = vec(2);
-        return baseline;
-    }
-
-    void setEpoch    (MeasFrame* frame, MEpoch*     mepoch)     {frame->set(*mepoch);}
-    void setDirection(MeasFrame* frame, MDirection* mdirection) {frame->set(*mdirection);}
-    void setPosition (MeasFrame* frame, MPosition*  mposition)  {frame->set(*mposition);}
-
-    MEpoch* convertEpoch(MeasFrame* frame, MEpoch* mepoch, int newsys) {
-        return new MEpoch(MEpoch::Convert(*mepoch,MEpoch::Ref(newsys,*frame))());
-    }
-    MDirection* convertDirection(MeasFrame* frame, MDirection* mdirection, int newsys) {
-        return new MDirection(MDirection::Convert(*mdirection,MDirection::Ref(newsys,*frame))());
-    }
-    MPosition* convertPosition(MeasFrame* frame, MPosition* mposition, int newsys) {
-        return new MPosition(MPosition::Convert(*mposition,MPosition::Ref(newsys,*frame))());
-    }
-    MBaseline* convertBaseline(MeasFrame* frame, MBaseline* mbaseline, int newsys) {
-        return new MBaseline(MBaseline::Convert(*mbaseline,MBaseline::Ref(newsys,*frame))());
+    Baseline convertBaseline(Baseline* input, int newsys, ReferenceFrame* frame) {
+        MBaseline input_baseline = getMBaseline(*input);
+        MeasFrame mframe = getMeasFrame(*frame);
+        MBaseline::Ref ref = MBaseline::Ref(newsys, mframe);
+        MBaseline output_baseline = MBaseline::Convert(input_baseline, ref)();
+        return getBaseline(output_baseline);
     }
 
     bool observatory(Position* position, char* name) {
